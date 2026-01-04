bc6c3c48aaa60b5200a011d3bd6dacf30f81d6e9
hussainali003 <hussainali9203@gmail.com>
2026-01-04 19:10:21 +0500

    delete: code lines file

--- src/pages/about/components/bioFile/index.jsx
+++ src/pages/about/components/bioFile/index.jsx
@@ -1,12 +1,8 @@
-import CodeLines from "../codeLines";
-
 import BioAbout from "./components/BioAbout";
 
 export default function BioFile() {
     return (
-        <div className="flex h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-900 hover:scrollbar-thumb-gray-400">
-            {/* code lines  */}
-            <CodeLines number={35}/>
+        <div className="flex px-4 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-900 hover:scrollbar-thumb-gray-400">
             {/* code */}
             <BioAbout />
         </div>
--- src/pages/about/components/skillsFile/index.jsx
+++ src/pages/about/components/skillsFile/index.jsx
@@ -1,12 +1,8 @@
-import CodeLines from "../codeLines";
-
 import SkillsAbout from "./components/SkillsAbout";
 
 export default function SkillsFile() {
     return (
-        <div className="h-full flex overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-900 hover:scrollbar-thumb-gray-400">
-            {/* code lines  */}
-            <CodeLines number={35}/>
+        <div className="flex px-4 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-900 hover:scrollbar-thumb-gray-400">
             {/* code */}
             <SkillsAbout />
         </div>
